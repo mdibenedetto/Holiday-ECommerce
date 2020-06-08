4a6543e7602d041124ffc6c292bcefba82a54e05
michele dibenedetto <michele.dibe78@gmail.com>
2020-06-08 23:23:36 +0100

    hot ifx - tool bar + carousel

--- js/common.js
+++ js/common.js
@@ -1,6 +1,4 @@
-const { body, head } = document;
-
-const toolBarar = `
+const toolBar = `
    <header class="header-nav-bar">
       <nav class="nav-bar">
         <ul>
@@ -15,21 +13,24 @@ const toolBarar = `
 `;
 const footer = ``;
 
-body.insertAdjacentHTML("afterbegin", toolBarar);
-body.insertAdjacentHTML("beforeend", footer);
+const scriptLinks = `
+<!-- CSS only -->
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
 
-head.insertAdjacentHTML(
-  "afterbegin",
-  `
-        <!-- CSS only -->
-      <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+<!-- JS, Popper.js, and jQuery -->
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+<link rel="stylesheet" href="./css/index.css" />
+`;
 
-      <!-- JS, Popper.js, and jQuery -->
-      <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-      <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-      <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
-        <link rel="stylesheet" href="./css/index.css" />
-        `
+document.head.insertAdjacentHTML("afterbegin", scriptLinks);
+// body.insertAdjacentHTML("beforeend", footer);
+
+document.addEventListener(
+  "DOMContentLoaded",
+  () => document.body.insertAdjacentHTML("afterbegin", toolBar),
+  false
 );
 
 // const links = document.querySelectorAll("a");
