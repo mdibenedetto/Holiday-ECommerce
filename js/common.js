74e849ceee923522c16a18cbe1659145493b7b94
michele dibenedetto <michele.dibe78@gmail.com>
2020-06-08 21:16:55 +0100

    added carousel home page

--- js/common.js
+++ js/common.js
@@ -1,7 +1,4 @@
-const {
-  body,
-  head,
-} = document;
+const { body, head } = document;
 
 const toolBarar = `
    <header class="header-nav-bar">
@@ -21,17 +18,20 @@ const footer = ``;
 body.insertAdjacentHTML("afterbegin", toolBarar);
 body.insertAdjacentHTML("beforeend", footer);
 
-head.insertAdjacentHTML("afterbegin",
-    `<link
-        rel="stylesheet"
-        href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css"
-        integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk"
-        crossorigin="anonymous"
-        />
-        <link rel="stylesheet" href="./css/index.css" />
-        `);
+head.insertAdjacentHTML(
+  "afterbegin",
+  `
+        <!-- CSS only -->
+      <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
 
-   
+      <!-- JS, Popper.js, and jQuery -->
+      <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+      <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+      <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+        <link rel="stylesheet" href="./css/index.css" />
+        `
+);
+
 // const links = document.querySelectorAll("a");
 // links.forEach((a) => a.addEventListener("click", (e) => e.preventDefault()));
 
