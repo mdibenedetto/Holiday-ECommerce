ec12537e1c37b0f07ea920cd8d79c6dca0a3a39a
michele dibenedetto <michele.dibe78@gmail.com>
2020-06-10 13:54:26 +0100

    navbar refactoring; added fake login;

--- js/common.js
+++ js/common.js
@@ -2,33 +2,118 @@ const pageLoader = `<div class="loader-container">
   <div class="loader"></div>
 </div>`;
 
+let usetToolbar = ``;
+
+window.currentUser = getCurrentUser();
+if (window.currentUser) {
+  // usetToolbar = `<li>Welcome ${window.currentUser.userName}</li>
+  // <li><a href="login.html" onclick="logout()">Log out</a></li>`;
+ 
+  usetToolbar = `    
+    <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" 
+            style = "width: auto;"
+            href="#" 
+            id="navbarDropdownMenuLink" 
+            role="button" 
+            data-toggle="dropdown"
+            aria-haspopup="true" 
+            aria-expanded="false">
+          Welcome ${window.currentUser.userName}
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+          <a class="dropdown-item" >Account</a>
+          <a class="dropdown-item" >Order history</a>
+          <a class="dropdown-item" >Cart</a>
+          <a class="dropdown-item" href="login.html" onclick="logout()">
+            Log out
+          </a>
+        </div>
+      </li>
+    `;
+} else {
+  // usetToolbar = `<li><a href="login.html">Login</a></li>
+  // <li><a href="register.html">Register</a></li>`;
+
+  usetToolbar = ` <li class="nav-item">
+        <a class="nav-link" href="login.html">
+          Login
+        </a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="register.html">
+          Register
+        </a>
+      </li>`;
+}
+
+{
+  /* <nav class="navbar nav-bar">
+<ul>
+  <li><a href="index.html">Home</a></li>
+  <li><a href="holiday.html">Holidays</a></li>
+  <li><a href="about.html">About</a></li>
+  ${usetToolbar}
+</ul>
+</nav> */
+}
 const toolBar = `
-   <header class="header-nav-bar">
-      <nav class="nav-bar">
-        <ul>
-          <li><a href="index.html">Home</a></li>
-          <li><a href="holiday.html">Holidays</a></li>
-          <li><a href="about.html">About</a></li>
-          <li><a href="login.html">Login</a></li>
-          <li><a href="register.html">Register</a></li>
-        </ul>
-      </nav>
+   <header class="header-navbar container">
+    <nav class="navbar-background navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand">Cutravel</a>
+        <button class="navbar-toggler" 
+            type="button" 
+            data-toggle="collapse" 
+            data-target="#navbarNavDropdown" 
+            aria-controls="navbarNavDropdown" 
+            aria-expanded="false" 
+            aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavDropdown">
+          <ul class="navbar-nav">
+            <li class="nav-item active"> 
+              <a class="nav-link" href="index.html">
+                Home <span class="sr-only">(current)</span>
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="holiday.html">
+                Holidays
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="about.html">
+                About
+              </a>
+            </li>
+            </ul>
+            
+            <ul class="navbar-nav ml-auto">
+            ${usetToolbar}  
+            </ul>
+          
+        </div>
+      </nav>    
     </header>
 `;
-const footer = ``;
 
-const scriptLinks = `
-<!-- CSS only -->
-<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+ 
 
-<!-- JS, Popper.js, and jQuery -->
-<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
-<link rel="stylesheet" href="./css/index.css" />
-`;
+// const footer = ``;
 
-document.head.insertAdjacentHTML("afterbegin", scriptLinks);
+// const scriptLinks = `
+// <!-- CSS only -->
+// <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+
+// <!-- JS, Popper.js, and jQuery -->
+// <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+// <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+// <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+// <link rel="stylesheet" href="./css/index.css" />
+// `;
+
+// document.head.insertAdjacentHTML("afterbegin", scriptLinks);
 // body.insertAdjacentHTML("beforeend", footer);
 
 document.addEventListener(
@@ -47,12 +132,32 @@ function initLayoutPage() {
 }
 
 function setLinkListener() {
-  const links = document.querySelectorAll("nav a");
+  const links = document.querySelectorAll("nav a:not(.navbar-brand):not([role=button])");
   links.forEach((a) =>
     a.addEventListener("click", (e) => {
       document.body.setAttribute("data-loading", "true");
     })
   );
+}
+
+// TODO change when move to server side coding
+function setCurrentUser(user) {
+  localStorage.setItem("currentUser", JSON.stringify(user));
+}
+// TODO change when move to server side coding
+function getCurrentUser() {
+  const value = localStorage.getItem("currentUser");
+  let currentUser = {};
+  if (value) currentUser = JSON.parse(value);
+  else currentUser = undefined;
+
+  return currentUser;
+}
+
+// TODO change when move to server side coding
+function logout() {
+  localStorage.setItem("currentUser", null);
+  // window.location = "/login.html";
 }
 
 // let currentIndex = 0;
---
+++ js/login.js
@@ -0,0 +1,16 @@
+window.currentUser = null;
+
+const form = document.querySelector("form");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const userName = form.querySelector("[type=email]").value;
+
+  // TODO change when move to server side coding
+  setCurrentUser({
+    userName,
+  });
+  window.location = "/holiday.html";
+});
+
