081fff5d03a8f6cee353fefa1c418178240daa05
michele dibenedetto <michele.dibe78@gmail.com>
2020-06-21 11:46:00 +0100

    create VStudio c# project blueprint

---
+++ src/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace nci_ss_dream_holiday.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
