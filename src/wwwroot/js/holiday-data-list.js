37886007cdf026b865d833c3578f1209d385a9cd
michele dibenedetto <michele.dibe78@gmail.com>
2020-06-25 18:14:44 +0100

    added new pages LOGIN, HOLIDAY

---
+++ src/wwwroot/js/holiday-data-list.js
@@ -0,0 +1,83 @@
+﻿const baseImageUrl = `./img/holiday`;
+const holidayItems = [
+    {
+        id: 1,
+        imageUrl: baseImageUrl + "/barcelona.jpg",
+        title: "Barcelona",
+        price: 2000,
+        content: "Nulla vitae elit libero, a pharetra augue mollis interdum.",
+    },
+    {
+        id: 2,
+        imageUrl: baseImageUrl + "/moscow.jpg",
+        title: "Moscow",
+        price: 1600,
+        content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    },
+    {
+        id: 3,
+        imageUrl: baseImageUrl + "/thailand.jpg",
+        title: "Thailand",
+        price: 1000,
+        content:
+            "Praesent commodo cursus magna, vel scelerisque nisl consectetur.",
+    },
+    {
+        id: 4,
+        imageUrl: baseImageUrl + "/new_zealand.jpg",
+        title: "New Zealand",
+        price: 2000,
+        content: "Nulla vitae elit libero, a pharetra augue mollis interdum.",
+    },
+    {
+        id: 5,
+        imageUrl: baseImageUrl + "/goa.jpg",
+        title: "Goa",
+        price: 1600,
+        content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    },
+    {
+        id: 6,
+        imageUrl: baseImageUrl + "/france.jpg",
+        title: "France",
+        price: 1000,
+        content:
+            "Praesent commodo cursus magna, vel scelerisque nisl consectetur.",
+    },
+    {
+        id: 7,
+        imageUrl: baseImageUrl + "/canada.jpg",
+        title: "Canada",
+        price: 2000,
+        content: "Nulla vitae elit libero, a pharetra augue mollis interdum.",
+    },
+    {
+        id: 8,
+        imageUrl: baseImageUrl + "/turkey.jpg",
+        title: "Turkey",
+        price: 1600,
+        content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    },
+    {
+        id: 9,
+        imageUrl: baseImageUrl + "/egypt.jpg",
+        title: "Egypt",
+        price: 1000,
+        content:
+            "Praesent commodo cursus magna, vel scelerisque nisl consectetur.",
+    },
+    {
+        id: 10,
+        imageUrl: baseImageUrl + "/japan.jpg",
+        title: "Japan",
+        price: 2000,
+        content: "Nulla vitae elit libero, a pharetra augue mollis interdum.",
+    },
+    {
+        id: 11,
+        imageUrl: baseImageUrl + "/brazil.jpg",
+        title: "Brazil",
+        price: 1600,
+        content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    },
+];
---
+++ src/wwwroot/js/login.js
@@ -0,0 +1,17 @@
+﻿window.currentUser = null;
+
+const form = document.querySelector("form");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const userName = form.querySelector("[type=email]").value;
+
+  // TODO change when move to server side coding
+  setCurrentUser({
+    userName,
+  });
+  //window.location = "/holiday";
+});
+
+ß
