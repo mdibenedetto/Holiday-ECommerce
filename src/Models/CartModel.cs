a84421d0607403a9005b3b5ea4d71c13034cca02
michele dibenedetto <michele.dibe78@gmail.com>
2020-06-29 16:23:11 +0100

    Added Cart  Model

---
+++ src/Models/CartModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace dream_holiday.Models
+{
+    public class CartModel
+    {
+        public CartModel()
+        {
+        }
+
+        public String Title = "";
+        public String Description = "";
+        public bool IsInstock = false;
+        public int Qty = 0;
+        public Decimal price = 0;
+      }
+}
