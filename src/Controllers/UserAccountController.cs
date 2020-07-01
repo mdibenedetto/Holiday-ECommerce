e122ab7f4a035e212bb1c7bf8c71a6a504f9b109
petrancirl <petrancirl@gmail.com>
2020-07-01 21:52:38 +0100

    MockData UserAccount

--- src/Controllers/UserAccountController.cs
+++ src/Controllers/UserAccountController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using dream_holiday.Models;
 
 namespace dream_holiday.Controllers
 {
@@ -11,7 +12,35 @@ namespace dream_holiday.Controllers
     {
         public IActionResult Index()
         {
-            return View();
+            var user = GetData();
+            return View(user);
+        }
+
+        private UserAccountModel GetData()
+        {
+            var user = new UserAccountModel();
+            user.FirstName = "Petra";
+            user.LastName = "Furkes";
+            user.Email = "firstname.lastname@gmail.com";
+            user.Paswoord = "pasword123";
+            user.RetypePasword = "pasword123";
+            user.Country = "Ireland";
+            user.Address = "Apt.25, Street 28";
+            user.Address2 = "Dublin 1";
+            user.Town = "Dublin";
+            user.County = "Co. Dublin";
+            user.Telephone = "123456789";
+            user.FirstNameCard = "Petra";
+            user.LastNameCard = "Furkes";
+            user.CardNumber = "9842 4520 1111 2222";
+            user.CardCVC = "228";
+            user.CountryBilling = "Ireland";
+            user.AddressBilling = "Apt. 25, street 28";
+            user.Address2Billing = "Dublin 1";
+            user.TownBilling = "Dublin";
+            user.County2Billing = "Co. Dublin";
+
+            return user;
         }
     }
 }
---
+++ src/Models/UserAccountModel.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace dream_holiday.Models
+{
+    public class UserAccountModel
+    {
+        public UserAccountModel() 
+        { 
+        }
+
+        public String FirstName = "";
+        public String LastName = "";
+        public String Email = "";
+        public String Paswoord = "";
+        public String RetypePasword = "";
+        public String Country = "";
+        public String Address = "";
+        public String Address2 = "";
+        public String Town = "";
+        public String County = "";
+        public String Telephone = "";
+        public String FirstNameCard = "";
+        public String LastNameCard = "";
+        public String CardNumber = "";
+        public String CardCVC = "";
+        public String CountryBilling = "";
+        public String AddressBilling = "";
+        public String Address2Billing = "";
+        public String TownBilling = "";
+        public String County2Billing = "";
+
+
+
+        public String GetFullName()
+        {
+            return FirstName + " " + LastName;
+       
+        }
+
+        public String GetFullNameCard()
+        {
+            return FirstNameCard + " " + LastNameCard;
+
+        }
+
+    }
+}
