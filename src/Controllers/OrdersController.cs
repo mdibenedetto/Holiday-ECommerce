b94d2b353a4797c86805192949860dfbbccd56ab
Raminta <56090053+ramintaquiet@users.noreply.github.com>
2020-07-02 09:33:23 +0100

    fake data in Orders

--- src/Controllers/OrdersController.cs
+++ src/Controllers/OrdersController.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using dream_holiday.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualBasic;
 
 namespace dream_holiday.Controllers
 {
@@ -10,7 +12,32 @@ namespace dream_holiday.Controllers
     {
         public IActionResult Index()
         {
-            return View();
+            List<OrdersModel> banana = GetData();
+            return View(banana);
+        }
+
+        private List<OrdersModel> GetData()
+        {
+            var list = new List<OrdersModel>();
+
+            for (var i = 0; i < 10; i++)
+            {
+                list.Add(new OrdersModel
+                {
+
+                    nr = i + 1,
+                    Id = 125478 + i,
+                    Date = DateTime.Now.AddDays(i),
+                   // .Now.AddDays(i),
+                    Price = (decimal)1299.99 + (i * 2),
+                    Qty = 1,
+                    Status = i % 2 == 0
+                }); ; ; ; ;
+ 
+            }
+                     return list;
+            //---
+
         }
     }
 }
