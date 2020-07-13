86d743931d0fa61781a6406f78cad87829e0653a
michele dibenedetto <michele.dibe78@gmail.com>
2020-07-13 19:01:17 +0100

    added travel package table and page

--- src/Controllers/HolidayController.cs
+++ src/Controllers/HolidayController.cs
@@ -1,19 +1,136 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
+
+using dream_holiday.Data;
+using dream_holiday.Models;
+
 using Microsoft.AspNetCore.Mvc;
 
-// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
-namespace dream_holiday.Controllers
+namespace dream_holIday.Controllers
 {
-    public class HolidayController : Controller
+    public class HolIdayController : Controller
     {
-        // GET: /<controller>/
+        private readonly ApplicationDbContext _context;
+
+        public HolIdayController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult Index()
         {
+            // todo= remove it when cart is ready
+            this.MockData();
+
             return View();
         }
+
+        private void MockData()
+        {
+            if (_context.TravelPackage.Any())
+            {
+                return;
+            }
+
+            var list = buildList(); 
+            _context.TravelPackage.AddRange(list);
+            _context.SaveChanges();
+
+        }
+
+
+        List<TravelPackage> buildList()
+        {
+            var base_Image_Url = "./img/holIday";
+
+            List<TravelPackage> holidayItems =
+                new List<TravelPackage> {
+                    new TravelPackage{
+                        Image = base_Image_Url + "/barcelona.jpg",
+                        Name = "Barcelona",
+                        Price= 2000,
+                        Description= "Nulla vitae elit libero, a pharetra augue mollis interdum.",
+                    },
+                    new TravelPackage  {
+
+                        Image= base_Image_Url + "/moscow.jpg",
+                        Name= "Moscow",
+                        Price= 1600,
+                        Description= "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+                    },
+                    new TravelPackage {
+
+                        Image= base_Image_Url + "/thailand.jpg",
+                        Name= "Thailand",
+                        Price= 1000,
+                        Description=
+                                "Praesent commodo cursus magna, vel scelerisque nisl consectetur.",
+                    },
+                    new TravelPackage  {
+
+                        Image= base_Image_Url + "/new_zealand.jpg",
+                        Name= "New Zealand",
+                        Price= 2000,
+                        Description= "Nulla vitae elit libero, a pharetra augue mollis interdum.",
+                    },
+                    new TravelPackage  {
+
+                        Image= base_Image_Url + "/goa.jpg",
+                        Name= "Goa",
+                        Price= 1600,
+                        Description= "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+                    },
+                    new TravelPackage  {
+
+                        Image= base_Image_Url + "/france.jpg",
+                        Name= "France",
+                        Price= 1000,
+                        Description=
+                                "Praesent commodo cursus magna, vel scelerisque nisl consectetur.",
+                    },
+                        new TravelPackage{
+
+                        Image= base_Image_Url + "/canada.jpg",
+                        Name= "Canada",
+                        Price= 2000,
+                        Description= "Nulla vitae elit libero, a pharetra augue mollis interdum.",
+                    },
+                    new TravelPackage  {
+
+                        Image= base_Image_Url + "/turkey.jpg",
+                        Name= "Turkey",
+                        Price= 1600,
+                        Description= "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+                    },
+                    new TravelPackage {
+
+                        Image= base_Image_Url + "/egypt.jpg",
+                        Name= "Egypt",
+                        Price= 1000,
+                        Description=
+                                "Praesent commodo cursus magna, vel scelerisque nisl consectetur.",
+                    },
+                    new TravelPackage {
+
+                        Image= base_Image_Url + "/japan.jpg",
+                        Name= "Japan",
+                        Price= 2000,
+                        Description= "Nulla vitae elit libero, a pharetra augue mollis interdum.",
+                    },
+                    new TravelPackage {
+
+                        Image= base_Image_Url + "/brazil.jpg",
+                        Name= "Brazil",
+                        Price= 1600,
+                        Description= "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+                    }
+                };
+            return holidayItems;
+
+        }
     }
+
+
 }
