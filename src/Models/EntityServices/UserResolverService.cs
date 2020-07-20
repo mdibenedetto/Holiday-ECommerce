0c2cf05169ea8e2831f115aeef25b82af5e9abc6
michele dibenedetto <michele.dibe78@gmail.com>
2020-07-20 21:34:39 +0100

    add cotext  user  resolver

---
+++ src/Models/EntityServices/UserResolverService.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+
+namespace dream_holiday.Models.EntityServices
+{
+    public class UserResolverService
+    {
+        private readonly IHttpContextAccessor _httpContext;
+        private readonly UserManager<ApplicationUser> _userManager;
+        
+        public UserResolverService(IHttpContextAccessor httpContext,
+                                    UserManager<ApplicationUser> userManager)
+        {
+            _httpContext = httpContext;
+            _userManager = userManager;
+        }
+
+        public UserManager<ApplicationUser>  getUserManager()
+        {
+            return _userManager;
+        }
+
+        public IHttpContextAccessor getHTTPContext()
+        {
+            return _httpContext;
+        }
+
+        //public async string GetUser()
+        //{
+        //    return await _context.HttpContext?.User?.Identity?.Name;
+        //}
+    }
+}
